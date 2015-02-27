ff6243377cf1639fc0744682b036df50e3a54ccb
thawk <thawk@mail.com>
2015-02-28 00:04:51 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ln -sf .tmux.conf ~/
+ln -sf .screenrc ~/
+ln -sf .dir_colors ~/
+ln -sf .inputrc ~/
+ln -sf .mime.types ~/
+ln -sf .bashrc ~/
+ln -sf .ctags ~/
+ln -sf .bash_profile ~/
+
