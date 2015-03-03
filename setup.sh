c9d5e19a27a40e2f5bb485447f2782888419cf94
thawk009 <thawk009@gmail.com>
2015-03-03 11:32:56 +0800

    在setup.sh中，正确ln

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-ln -sf .tmux.conf ~/
-ln -sf .screenrc ~/
-ln -sf .dir_colors ~/
-ln -sf .inputrc ~/
-ln -sf .mime.types ~/
-ln -sf .bashrc ~/
-ln -sf .ctags ~/
-ln -sf .bash_profile ~/
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+ln -sf "${SCRIPT_DIR}/.tmux.conf" ~/
+ln -sf "${SCRIPT_DIR}/.screenrc" ~/
+ln -sf "${SCRIPT_DIR}/.dir_colors" ~/
+ln -sf "${SCRIPT_DIR}/.inputrc" ~/
+ln -sf "${SCRIPT_DIR}/.mime.types" ~/
+ln -sf "${SCRIPT_DIR}/.bashrc" ~/
+ln -sf "${SCRIPT_DIR}/.ctags" ~/
+ln -sf "${SCRIPT_DIR}/.bash_profile" ~/
 
