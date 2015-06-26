c5675c65d4e410c00f6a5b7722536308f36a5111
thawk <thawk@mail.com>
2015-06-26 09:54:50 +0800

    增加bash_completion

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,7 @@ ln -sf "${SCRIPT_DIR}/.mime.types" ~/
 ln -sf "${SCRIPT_DIR}/.bashrc" ~/
 ln -sf "${SCRIPT_DIR}/.ctags" ~/
 ln -sf "${SCRIPT_DIR}/.bash_profile" ~/
+ln -sf "${SCRIPT_DIR}/.bash_completion" ~/
+[ -e ~/.bash_completion.d ] || mkdir ~/.bash_completion.d
 [ -e ~/libexec ] || ln -sf "${SCRIPT_DIR}/libexec" ~/
 
