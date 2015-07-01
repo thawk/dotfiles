a1e9295354380355e833d4ca34a86436ddd0a917
thawk <mymail>
2015-07-01 17:03:28 +0800

    增加minttyrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ ln -sf "${SCRIPT_DIR}/.inputrc" ~/
 ln -sf "${SCRIPT_DIR}/.mime.types" ~/
 ln -sf "${SCRIPT_DIR}/.bashrc" ~/
 ln -sf "${SCRIPT_DIR}/.ctags" ~/
+ln -sf "${SCRIPT_DIR}/.minttyrc" ~/
 ln -sf "${SCRIPT_DIR}/.bash_profile" ~/
 [ -e ~/libexec ] || ln -sf "${SCRIPT_DIR}/libexec" ~/
 
