9d4d672e04c3897f5e4c514d933585b4c50e45e3
thawk <thawk009@gmail.com>
2016-10-05 11:30:41 +0800

    export SSH_AUTH_SOCK only in WSL, but export even if keeagent is not start yet

--- keeagent/keeagent.sh
+++ keeagent/keeagent.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env sh
 
+# only needed for WSL
+if ! [ "$(uname -s)" = "Linux" ] || ! grep "Microsoft\|WSL" /proc/sys/kernel/osrelease > /dev/null
+then
+    exit 0
+fi
+
 # If MSYSGIT socket in keeagent is set as c:\Users/foo/Documents/ssh_auth_msysgit
 SSH_AUTH_KEEAGENT_SOCK=/mnt/c/Users/$USER/keepass.sock
+
+# Don't overwrite SSH_AUTH_SOCK
+if [ -n "${SSH_AUTH_SOCK}" ]
+then
+    unset SSH_AUTH_KEEAGENT_SOCK
+    exit 0
+fi
+
+# export SSH_AUTH_SOCK even SSH_AUTH_KEEAGENT_SOCK does not exist.
+# this allow SSH_KEEAGENT work when keeagent is started.
+export SSH_AUTH_SOCK="/tmp/keeagent.sock"
 
 if [ -e "$SSH_AUTH_KEEAGENT_SOCK" ]
 then
     SSH_AUTH_KEEAGENT_PORT=`sed -r 's/!<socket >([0-9]*\b).*/\1/' ${SSH_AUTH_KEEAGENT_SOCK}`
 
-    #use socket filename structure similar to ssh-agent
-    #ssh_auth_tmpdir=`mktemp --tmpdir --directory keeagent-ssh.XXXXXXXXXX`
-    #export SSH_AUTH_SOCK="${ssh_auth_tmpdir}/agent.$$"
-    export SSH_AUTH_SOCK="/tmp/keeagent.sock"
     SSH_AUTH_LOCK_FILE=/var/lock/keeagent.lock
 
     # remove sock file if only sock exists, and lock file is not exists.
