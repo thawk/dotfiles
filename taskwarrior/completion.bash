7919f67c89444d550613beef77011f974be60c3d
thawk <thawk009@gmail.com>
2016-11-02 21:42:02 +0800

    使用taskwarrior自带的_task

---
+++ taskwarrior/completion.bash
@@ -0,0 +1,6 @@
+if [ -f /usr/local/share/doc/task/scripts/bash/task.sh ]; then
+    source /usr/local/share/doc/task/scripts/bash/task.sh
+elif [ -f /usr/share/doc/task/scripts/bash/task.sh ]; then
+    source /usr/share/doc/task/scripts/bash/task.sh
+fi
+
---
+++ taskwarrior/env.zsh
@@ -0,0 +1,6 @@
+if [ -f /usr/local/share/doc/task/scripts/zsh/_task ]; then
+    fpath=($fpath /usr/local/share/doc/task/scripts/zsh)
+elif [ -f /usr/share/doc/task/scripts/zsh/_task ]; then
+    fpath=($fpath /usr/share/doc/task/scripts/zsh)
+fi
+
