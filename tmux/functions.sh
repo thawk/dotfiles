098171765542d1c49b920bd05f9624f5f6bfbb02
thawk <thawk009@gmail.com>
2016-12-06 17:18:52 +0800

    update tm

--- tmux/functions.sh
+++ tmux/functions.sh
@@ -1,10 +1,21 @@
-# 打开指定名称的session
+# # 打开指定名称的session
 # tm() {
 #     first_session="$1"
 #
 #     while [ ! -z "$1" ]
 #     do
-#         # 记录
+#         [ -z "$first_session" ] && first_session="$1"
+#         tmxu has-session -t "$1" 2> /dev/null
+#         if [ $? != 0 ]
+#         then
+#             tmux new -d -s "$1"
+#         fi
+#         shift
 #     done
+#
+#     if [ ! -z "$first_session" ]
+#     then
+#         tmux attach -t "$first_session"
+#     fi
 # }
 
