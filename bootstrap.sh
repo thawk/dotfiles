7efcf360047af18e75d9fe0a8c8812b248dfc099
thawk <thawk009@gmail.com>
2016-12-14 10:48:38 +0800

    SKIP和OK用不同的颜色

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,10 @@ success () {
     printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
+skip () {
+    printf "\r\033[2K  [\033[00;35mSKIP\033[0m] $1\n"
+}
+
 fail () {
     printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
     echo ''
@@ -152,7 +156,7 @@ link_file () {
 
         if [ "$skip" == "true" ]
         then
-            success "skipped $src"
+            skip "skipped $src"
         fi
     fi
 
