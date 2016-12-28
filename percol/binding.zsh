7969cb01fc3265d080ec60e143b6c0db8c97472c
thawk <thawk009@gmail.com>
2016-12-28 16:35:42 +0800

    percol的函数名前加_,避免影响命令行补全

--- percol/binding.zsh
+++ percol/binding.zsh
@@ -1,11 +1,12 @@
 if which percol &> /dev/null; then
-    function percol_select_history() {        local tac
+    function _percol_select_history() {
+        local tac
         (which gtac &> /dev/null) && tac="gtac" || { (which tac &> /dev/null) && tac="tac" || { tac="tail -r" } }
         BUFFER=$(fc -l -n 1 | eval $tac | percol --query "$LBUFFER")
         CURSOR=$#BUFFER         # move cursor
         zle -R -c               # refresh
     }
 
-    zle -N percol_select_history
-    bindkey '^R' percol_select_history
+    zle -N _percol_select_history
+    bindkey '^R' _percol_select_history
 fi
