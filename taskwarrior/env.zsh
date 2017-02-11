2a503610b8ca687ad41dd464954c0c88cf85cd9c
thawk <thawk009@gmail.com>
2017-02-11 16:26:03 +0800

    update

--- taskwarrior/env.zsh
+++ taskwarrior/env.zsh
@@ -1,6 +1,7 @@
-if [ -f /usr/local/share/doc/task/scripts/zsh/_task ]; then
+if [ -f /usr/local/share/taskwarrior/scripts/zsh/_task ]; then
+    fpath=($fpath /usr/local/share/taskwarrior/scripts/zsh)
+elif [ -f /usr/local/share/doc/task/scripts/zsh/_task ]; then
     fpath=($fpath /usr/local/share/doc/task/scripts/zsh)
 elif [ -f /usr/share/doc/task/scripts/zsh/_task ]; then
     fpath=($fpath /usr/share/doc/task/scripts/zsh)
 fi
-
