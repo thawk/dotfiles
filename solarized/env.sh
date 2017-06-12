babf664a1123fc19b7911ea38ad3b7435b3052d6
thawk <thawk009@gmail.com>
2017-06-12 11:55:12 +0800

    把重置solarzied theme的工作从init.sh前移到env.sh，以便在tmux运行前就可以完成

--- solarized/env.sh
+++ solarized/env.sh
@@ -1,1 +1,8 @@
-export DYNAMIC_COLORS_ROOT="${DOTFILES_ROOT}/dynamic-colors/dynamic-colors"
+export DYNAMIC_COLORS_ROOT="${DOTFILES_ROOT}/solarized/dynamic-colors"
+
+if [[ "$DOTFILES_THEME" == "solarized" ]]
+then
+    echo "reset dynamic-colors to solarized" >> /tmp/dotfiles.log 
+    # 在顶层SHELL中恢复终端配色 
+    [ "$SHLVL" -eq 1 ] &&  dynamic-colors init
+fi
