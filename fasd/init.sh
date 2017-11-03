a88bcb4f28ba324fe8e1d46837d997486df027a4
thawk <thawk009@gmail.com>
2017-11-03 13:05:09 +0800

    修正fasd生成的init脚本错误的问题；修正bash的提示符错误问题

--- fasd/init.sh
+++ fasd/init.sh
@@ -1,11 +1,12 @@
 fasd_cache_dir="${HOME}/.cache/fasd"
 [ ! -d "${fasd_cache_dir}" ] && mkdir -p "${fasd_cache_dir}"
 
-fasd_cache="${fasd_cache_dir}/fasd-init-${SHELL##*/}"
+fasd_shell="${SHELL##*/}"
+fasd_cache="${fasd_cache_dir}/fasd-init-${fasd_shell}"
 if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-    if [ "\$ZSH_VERSION" ] && compctl; then # zsh
+    if [ "$fasd_shell" == "zsh" ]; then # zsh
         fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install >| "$fasd_cache"
-    elif [ "\$BASH_VERSION" ] && complete; then # bash
+    elif [ "$fasd_shell" == "bash" ]; then # bash
         fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
     else # posix shell
         fasd --init posix-alias posix-hook >| "$fasd_cache"
@@ -13,5 +14,6 @@ if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
 fi
 
 source "$fasd_cache"
+unset fasd_shell
 unset fasd_cache
 unset fasd_cache_dir
