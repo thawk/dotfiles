94f2b8a6d8c41e21a539f52b0d37bc3f2e0d7f23
thawk <thawk009@gmail.com>
2018-05-09 10:41:49 +0800

    remove depends for /dev/stderr

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,33 +8,33 @@ DOTFILES_LOCAL="${HOME}/.dotfiles.local"
 set -e
 
 debug () {
-    # printf "\r  [ \033[00;34m..\033[0m ] $1\n" > /dev/stderr
+    # printf "\r  [ \033[00;34m..\033[0m ] $1\n" 1>&2
     echo > /dev/null
 }
 
 info () {
-    printf "\r  [ \033[00;34m..\033[0m ] $1\n" > /dev/stderr
+    printf "\r  [ \033[00;34m..\033[0m ] $1\n" 1>&2
 }
 
 user () {
-    printf "\r  [ \033[0;33m??\033[0m ] $1\n" > /dev/stderr
+    printf "\r  [ \033[0;33m??\033[0m ] $1\n" 1>&2
 }
 
 success () {
-    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n" > /dev/stderr
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n" 1>&2
 }
 
 warn () {
-    printf "\r\033[2K  [\033[00;33mWARN\033[0m] $1\n" > /dev/stderr
+    printf "\r\033[2K  [\033[00;33mWARN\033[0m] $1\n" 1>&2
 }
 
 skip () {
-    printf "\r\033[2K  [\033[00;35mSKIP\033[0m] $1\n" > /dev/stderr
+    printf "\r\033[2K  [\033[00;35mSKIP\033[0m] $1\n" 1>&2
     # echo > /dev/null
 }
 
 fail () {
-    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n" > /dev/stderr
+    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n" 1>&2
     echo ''
     exit
 }
