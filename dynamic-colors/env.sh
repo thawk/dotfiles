3b942fea4a5e0022b8d053dbfec17c3f26bdf0e7
thawk <thawk009@gmail.com>
2018-07-19 08:33:52 +0800

    remove unused echo

--- dynamic-colors/env.sh
+++ dynamic-colors/env.sh
@@ -2,7 +2,6 @@ export DYNAMIC_COLORS_ROOT="${DOTFILES_ROOT}/dynamic-colors/dynamic-colors"
 
 if [[ "$DOTFILES_THEME" == "solarized" ]]
 then
-    echo "reset dynamic-colors to solarized" >> /tmp/dotfiles.log 
     # 在顶层SHELL中恢复终端配色 
     [ "$SHLVL" -eq 1 ] &&  dynamic-colors init
 fi
