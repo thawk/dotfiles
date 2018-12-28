397b3286f9ad72b972a779007df601c932c65329
thawk <thawk009@gmail.com>
2018-12-28 18:25:41 +0800

    update dockers

--- my-dockers/alias-asciidoctor.sh
+++
@@ -1,6 +0,0 @@
-for cmd in asciidoctor asciidoctor-pdf asciidoctor-epub3
-do
-    if ! type ${cmd} &> /dev/null ; then
-        alias ${cmd}="docker run -it --rm -v \$(pwd):/documents/ asciidoctor/docker-asciidoctor ${cmd} -r asciidoctor-diagram"
-    fi
-done
