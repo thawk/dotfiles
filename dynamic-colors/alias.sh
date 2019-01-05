339da63dde33b58aa28f90ab50fc17301d1b5339
thawk <thawk009@gmail.com>
2019-01-05 11:10:22 +0800

    fix percol not work in mac

--- dynamic-colors/alias.sh
+++ dynamic-colors/alias.sh
@@ -1,5 +1,5 @@
 if type peco &> /dev/null ; then
-    alias dcc="dynamic-colors list | peco   --prompt='Colorscheme> ' | head -n 1 | xargs --no-run-if-empty dynamic-colors switch"
+    alias dcc="dynamic-colors list | peco   --prompt='Colorscheme> ' | head -n 1 | xargs dynamic-colors switch"
 elif type percol &> /dev/null ; then
-    alias dcc="dynamic-colors list | percol --prompt='Colorscheme> ' | head -n 1 | xargs --no-run-if-empty dynamic-colors switch"
+    alias dcc="dynamic-colors list | percol --prompt='Colorscheme> ' | head -n 1 | xargs dynamic-colors switch"
 fi
--- percol/requirements.sh
+++ percol/requirements.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # use peco if exists
-(type python2 &> /dev/null) && ! (type peco &> /dev/null)
+! (type peco &> /dev/null) && ((type python2 &> /dev/null) || (type python2.7 &> /dev/null))
