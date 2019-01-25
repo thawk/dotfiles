0f17dd8342f25e2fcb883af253fe69a10e91d12e
thawk <thawk009@gmail.com>
2019-01-25 22:04:48 +0800

    add colortest script; improve dynamic-colors dcc command

--- dynamic-colors/alias.sh
+++
@@ -1,5 +0,0 @@
-if type peco &> /dev/null ; then
-    alias dcc="dynamic-colors list | peco   --prompt='Colorscheme> ' | head -n 1 | xargs dynamic-colors switch"
-elif type percol &> /dev/null ; then
-    alias dcc="dynamic-colors list | percol --prompt='Colorscheme> ' | head -n 1 | xargs dynamic-colors switch"
-fi
---
+++ dynamic-colors/cmd.sh
@@ -0,0 +1,13 @@
+dcc() {
+if type peco &> /dev/null ; then
+    dynamic-colors list |
+     peco   --prompt='Colorscheme> ' --query="$1" |
+     head -n 1 |
+     xargs dynamic-colors switch
+elif type percol &> /dev/null ; then
+    dynamic-colors list |
+    percol --prompt='Colorscheme> ' --query="$1" |
+    head -n 1 |
+    xargs dynamic-colors switch
+fi
+}
