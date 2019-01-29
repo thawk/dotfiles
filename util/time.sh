e305e85a8f64999a441d3a8a6dbd322e2e3b391d
thawk <thawk009@gmail.com>
2019-01-29 14:12:25 +0800

    auto detect timestamp format (seconds/milliseconds/microseconds)

--- util/time.sh
+++ util/time.sh
@@ -1,26 +1,14 @@
 timestamp() {
     while [ $# -gt 0 ]
     do
-        timestamp=$1
-        date -d @$((timestamp / 1000000)) +"%Y-%m-%d %T".$((timestamp % 1000000))
-        shift
-    done
-}
-
-epochtime() {
-    while [ $# -gt 0 ]
-    do
-        epochtime=$1
-        date -d @$((epochtime / 1000)) +"%Y-%m-%d %T".$((epochtime % 1000))
-        shift
-    done
-}
-
-time_t() {
-    while [ $# -gt 0 ]
-    do
-        time_t=$1
-        date -d @$((time_t)) +"%Y-%m-%d %T"
+        ts=$1
+        if [[ $ts -lt 10000000000 ]]; then
+            date -d @$((ts)) +"%Y-%m-%d %T"
+        elif [[ $ts -lt 10000000000000 ]]; then
+            date -d @$((ts / 1000)) +"%Y-%m-%d %T".$((ts % 1000))
+        else
+            date -d @$((ts / 1000000)) +"%Y-%m-%d %T".$((ts % 1000000))
+        fi
         shift
     done
 }
