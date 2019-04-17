96c1388c1c63060106c242c64c05933e13c4feb3
thawk <thawk009@gmail.com>
2019-04-17 12:20:49 +0800

    fix timestamp util

--- util/time.sh
+++ util/time.sh
@@ -7,7 +7,7 @@ timestamp() {
         elif [[ $ts -lt 10000000000000 ]]; then
             date -d @$((ts / 1000)) +"%Y-%m-%d %T".$(printf "%03d" $((ts % 1000)))
         else
-            date -d @$((ts / 1000000)) +"%Y-%m-%d %T".$(printf "%03d" $((ts % 1000000)))
+            date -d @$((ts / 1000000)) +"%Y-%m-%d %T".$(printf "%06d" $((ts % 1000000)))
         fi
         shift
     done
