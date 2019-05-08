c5a4e62347db4937cb0aa0b15f7bcba5680b54f8
thawk <thawk009@gmail.com>
2019-05-08 15:53:51 +0800

    fix MacOS compatible

--- util/time.sh
+++ util/time.sh
@@ -1,14 +1,26 @@
 timestamp() {
+    local epoch=
+    local subsecs=
+
     while [ $# -gt 0 ]
     do
         ts=$1
         if [[ $ts -lt 10000000000 ]]; then
-            date -d @$((ts)) +"%Y-%m-%d %T"
+            epoch=ts
         elif [[ $ts -lt 10000000000000 ]]; then
-            date -d @$((ts / 1000)) +"%Y-%m-%d %T".$(printf "%03d" $((ts % 1000)))
+            epoch=$((ts / 1000))
+            subsecs=.$(printf "%03d" $((ts % 1000)))
         else
-            date -d @$((ts / 1000000)) +"%Y-%m-%d %T".$(printf "%06d" $((ts % 1000000)))
+            epoch=$((ts / 1000000))
+            subsecs=.$(printf "%06d" $((ts % 1000000)))
         fi
+
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+            date -r ${epoch} +"%Y-%m-%d %T"${subsecs}
+        else
+            date -d @${epoch} +"%Y-%m-%d %T"${subsecs}
+        fi
+
         shift
     done
 }
