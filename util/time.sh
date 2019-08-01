e24dff43099c81b14e774a9b9fc4f8a7b368db0c
thawk <thawk009@gmail.com>
2019-08-01 21:47:26 +0800

    timestamp display current time while no parameter is provided

--- util/time.sh
+++ util/time.sh
@@ -2,26 +2,34 @@ timestamp() {
     local epoch=
     local subsecs=
 
-    while [ $# -gt 0 ]
-    do
-        ts=$1
-        if [[ $ts -lt 10000000000 ]]; then
-            epoch=ts
-        elif [[ $ts -lt 10000000000000 ]]; then
-            epoch=$((ts / 1000))
-            subsecs=.$(printf "%03d" $((ts % 1000)))
-        else
-            epoch=$((ts / 1000000))
-            subsecs=.$(printf "%06d" $((ts % 1000000)))
-        fi
-
+    if [[ $# -eq 0 ]]; then
         if [[ "$OSTYPE" == "darwin"* ]]; then
-            date -r ${epoch} +"%Y-%m-%d %T"${subsecs}
+            date +"%s000000"
         else
-            date -d @${epoch} +"%Y-%m-%d %T"${subsecs}
+            date +"%s%N"
         fi
+    else
+        while [ $# -gt 0 ]
+        do
+            ts=$1
+            if [[ $ts -lt 10000000000 ]]; then
+                epoch=ts
+            elif [[ $ts -lt 10000000000000 ]]; then
+                epoch=$((ts / 1000))
+                subsecs=.$(printf "%03d" $((ts % 1000)))
+            else
+                epoch=$((ts / 1000000))
+                subsecs=.$(printf "%06d" $((ts % 1000000)))
+            fi
 
-        shift
-    done
+            if [[ "$OSTYPE" == "darwin"* ]]; then
+                date -r ${epoch} +"%Y-%m-%d %T"${subsecs}
+            else
+                date -d @${epoch} +"%Y-%m-%d %T"${subsecs}
+            fi
+
+            shift
+        done
+    fi
 }
 
