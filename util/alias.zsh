2180e7e286151e849daf89d8a38ca91e370939dd
thawk <thawk009@gmail.com>
2019-11-14 08:54:20 +0800

    change socks:// to socks5:// in 'ap'"

--- util/alias.zsh
+++ util/alias.zsh
@@ -1,3 +1,3 @@
 alias -g TODAY="\$(date +%Y-%m-%d)"
 # Use a local proxy
-alias ap="env all_proxy=socks://127.0.0.1:1080 GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -X 5 -x 127.0.0.1:1080 %h %p\"'"
+alias ap="env all_proxy=socks5://127.0.0.1:1080 GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -X 5 -x 127.0.0.1:1080 %h %p\"'"
