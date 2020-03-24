9911fd1d801d75a4d62036480836c51c5c7eeab3
thawk <thawk009@gmail.com>
2020-03-24 17:47:49 +0800

    update

--- util/bin/backup-spacevim.sh
+++ util/bin/backup-spacevim.sh
@@ -2,4 +2,4 @@
 
 DEST_FILE=${1:-spacevim.tar.bz2}
 
-tar -C "$HOME" -cjvf "${DEST_FILE}" --exclude .git --exclude "*.so" .SpaceVim.d .SpaceVim .cache/vimfiles
+tar -C "$HOME" -cjvf "${DEST_FILE}" --exclude .git --exclude "*.so" .SpaceVim.d .SpaceVim .cache/vimfiles .config/coc
