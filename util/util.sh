01cec31671adff2206b9f81d28a3c09e17209aab
thawk <thawk009@gmail.com>
2020-08-30 15:38:38 +0800

    update

--- util/util.sh
+++ util/util.sh
@@ -2,13 +2,49 @@ ansiesc() {
     sed -e 's/\[[0-9;]\+m//g'
 }
 
-vman () {
+vman() {
     vim +"set ft=man" +"Man $*"
 }
 
-setproxy () {
+setproxy() {
     # 如果命令行没有提供代理地址，使用MY_SOCKS5_PROXY的值，如果也没有设置就用127.0.0.1:1080
     local proxy=${1:-${MY_SOCKS5_PROXY:-127.0.0.1:1080}}
     export all_proxy=socks5://${MY_SOCKS5_PROXY:-127.0.0.1:1080}
     export GIT_SSH_COMMAND="ssh -o ProxyCommand=\"nc -X 5 -x ${MY_SOCKS5_PROXY:-127.0.0.1:1080} %h %p\""
+}
+
+cdd() {
+    if type tmux &> /dev/null
+    then
+        # 如果当前tmux session名称正好是$DOTFILES_SRC_ROOT下的目录名，则进入该目录
+        local proj_name="$(tmux display-message -p '#S')"
+        local src_root="${DOTFILES_SRC_ROOT:-$HOME/workspace}"
+    
+        if [[ -d "${src_root}/${proj_name}" ]]; then
+            cd "${src_root}/${proj_name}"
+            return
+        fi
+    fi
+    
+    # 没有找到与tmux session名称对应的项目，找当前目录往上的VCS目录作为项目的根目录
+    # 如果有多个VCS目录，说明存在一些子项目，取最顶层目录
+    local last_proj_root=
+    local dir="$PWD"
+    local last_dir=
+    
+    while [[ "$dir" != "$last_dir" ]]; do
+        for d in .git _darcs .hg .bzr .svn; do
+            if [[ -e "$dir/$d" ]]; then
+                last_proj_root="$dir"
+                break
+            fi
+        done
+    
+        last_dir="$dir"
+        dir="$(dirname "$dir")"
+    done
+    
+    if [[ -d "$last_proj_root" ]]; then
+        cd "$last_proj_root"
+    fi
 }
