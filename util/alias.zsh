fb3fe982709f7c0ea8a1872abf4343e162216627
thawk <thawk009@gmail.com>
2021-04-22 21:41:16 +0800

    update

--- util/alias.zsh
+++ util/alias.zsh
@@ -1,3 +1,4 @@
 alias -g TODAY="\$(date +%Y-%m-%d)"
 # Use a local proxy
 alias ap="env all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -X 5 -x \${MY_SOCKS5_PROXY:-127.0.0.1:1080} %h %p\"'"
+alias aph="env all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} "
