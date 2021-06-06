25feb552f8906d01b88832bba2a752d454b5721a
thawk <thawk009@gmail.com>
2021-06-06 23:28:18 +0800

    fix pyenv

--- pyenv/bootstrap.sh
+++ pyenv/bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH=$HOME/.pyenv/bin:$PATH
+
 conf_dir="${DOTFILES_LOCAL}/pyenv"
 mkdir -p "${conf_dir}"
 rm -f "${conf_dir}"/*
@@ -7,26 +9,31 @@ rm -f "${conf_dir}"/*
 env_file="${conf_dir}/env.sh"
 : > "${env_file}"
 
-echo "export PATH=\"$HOME/.pyenv/bin:\$PATH\"" >> "${env_file}"
-echo "export PYENV_VIRTUALENV_DISABLE_PROMPT=1" >> "${env_file}"
+path_file="${conf_dir}/path.sh"
+: > "${path_file}"
 
-#eval "$(pyenv init -)"
-#eval "$(pyenv virtualenv-init -)"
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> "${env_file}"
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> "${path_file}"
 
-# setup neovim pyenv
-if [[ -x "$HOME/.pyenv/versions/neovim2/bin/python" ]]; then
-    echo "export PYTHON_HOST_PROG=\"$HOME/.pyenv/versions/neovim2/bin/python\"" >> "${env_file}"
-fi
+pyenv init --path >> "${path_file}"
 
-if [[ -x "$HOME/.pyenv/versions/neovim3/bin/python" ]]; then
-    echo "export PYTHON3_HOST_PROG=\"$HOME/.pyenv/versions/neovim3/bin/python\"" >> "${env_file}"
-fi
-
-PATH=$HOME/.pyenv/bin:$PATH
+# set path to suppress the warning prompt of pyenv init -
+eval "$(pyenv init --path)"
 
 pyenv init - bash > "${conf_dir}/init.bash"
 pyenv virtualenv-init - bash > "${conf_dir}/virtualenv-init.bash"
 
 pyenv init - zsh > "${conf_dir}/init.zsh"
 pyenv virtualenv-init - zsh > "${conf_dir}/virtualenv-init.zsh"
+
+echo "export PYENV_VIRTUALENV_DISABLE_PROMPT=1" >> "${env_file}"
+
+# setup neovim pyenv
+if [[ -x "$HOME/.pyenv/versions/neovim2/bin/python" ]]; then
+    echo 'export PYTHON_HOST_PROG="$HOME/.pyenv/versions/neovim2/bin/python"' >> "${env_file}"
+fi
+
+if [[ -x "$HOME/.pyenv/versions/neovim3/bin/python" ]]; then
+    echo 'export PYTHON3_HOST_PROG="$HOME/.pyenv/versions/neovim3/bin/python"' >> "${env_file}"
+fi
 
