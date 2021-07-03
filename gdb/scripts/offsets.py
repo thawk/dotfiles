61b4b4bc4c629c9be48a3aa353a77fae52c1de7a
thawk <thawk009@gmail.com>
2021-07-03 17:50:55 +0800

    print field size and base class

--- gdb/scripts/offsets.py
+++ gdb/scripts/offsets.py
@@ -10,24 +10,33 @@ class Offsets(gdb.Command):
         if len(argv) != 1:
             raise gdb.GdbError('offsets-of take exactly 1 argument.')
 
-        stype = gdb.lookup_type(argv[0])
+        self.print_offsets(argv[0], gdb.lookup_type(argv[0]))
 
-        print(argv[0], '{')
+    def print_offsets(self, name, stype, prefix=""):
+        print('%s%s {' % (prefix, name))
         for field in stype.fields():
-            if hasattr(field, 'bitpos'):
+            if hasattr(field, 'bitpos') and field.bitpos is not None:
                 offset = '%4d (0x%04x)' % (field.bitpos//8, field.bitpos//8)
             else:
                 offset = '---- (0x----)'
+
+            if hasattr(field.type, 'sizeof') and field.type.sizeof is not None:
+                size = '%3d (0x%02x)' % (field.type.sizeof, field.type.sizeof)
+            else:
+                size = '---- (0x----)'
 
             if hasattr(field.type, 'name') and field.type.name:
                 typename = '   : ' + field.type.name
             else:
                 typename = ''
 
-            print('  %s => %s%s' % (offset, field.name, typename))
+            print('%s  %s / %s => %s%s' % (prefix, offset, size, field.name, typename))
 
-        print('}')
-        print('Total size: %d (0x%04x) bytes' % (stype.sizeof, stype.sizeof))
+            if field.is_base_class:
+                self.print_offsets(field.name, field.type, prefix + "     ")
+
+        print('%s}' % prefix)
+        print('%sTotal size: %d (0x%04x) bytes' % (prefix, stype.sizeof, stype.sizeof))
 
 Offsets()
 
