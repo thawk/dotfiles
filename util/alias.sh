426a343d8306e475552b614fb902cc10f6d4bbd0
thawk <thawk009@gmail.com>
2021-10-18 14:50:50 +0800

    update

--- util/alias.sh
+++ util/alias.sh
@@ -2,8 +2,8 @@
 alias ap="env all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -X 5 -x \${MY_SOCKS5_PROXY:-127.0.0.1:1080} %h %p\"' "
 alias aph="env all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} "
 # Use a local proxy
-alias apl="env all_proxy=socks5://-127.0.0.1:1080 GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -X 5 -x -127.0.0.1:1080 %h %p\"' "
-alias aphl="env all_proxy=socks5://-127.0.0.1:1080 "
+alias apl="env all_proxy=socks5://127.0.0.1:1080 GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -X 5 -x 127.0.0.1:1080 %h %p\"' "
+alias aphl="env all_proxy=socks5://127.0.0.1:1080 "
 # no proxy
 alias np="env all_proxy= GIT_SSH_COMMAND= "
 alias stripcolors='sed -r "s/\x1b\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"'
