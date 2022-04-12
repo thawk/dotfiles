988b04bd899aa5680a1199dc115532107894ecfc
thawk <thawk009@gmail.com>
2022-04-12 17:03:27 +0800

    Squashed 'dynamic-colors/dynamic-colors/' content from commit 6daec27
    git-subtree-dir: dynamic-colors/dynamic-colors
    git-subtree-split: 6daec27bb33a900a134162194d948697aa6a9e96

---
+++ colorschemes/multitude-dark.sh
@@ -0,0 +1,35 @@
+# Multitude colorscheme with dark background
+#
+# by Renato Alves (alves.rjc@gmail.com)
+
+background="#000000"
+foreground="#ffffff"
+cursor="#ffffff"
+mouse_background="#000000"
+mouse_foreground="#ffffff"
+highlight="#777777"
+border="#333333"
+
+ color0="#000000"  # black
+ color8="#666666"  # brblack
+
+ color1="#9e1828"  # red
+ color9="#cf6171"  # brred
+
+ color2="#008800"  # green
+color10="#7cbc8c"  # brgreen
+
+ color3="#968a38"  # yellow
+color11="#fff796"  # bryellow
+
+ color4="#414171"  # blue
+color12="#4186be"  # brblue
+
+ color5="#963c59"  # magenta
+color13="#cf9ebe"  # brmagenta
+
+ color6="#418179"  # cyan
+color14="#71bebe"  # brcyan
+
+ color7="#bebebe"  # white
+color15="#ffffff"  # brwhite
---
+++ colorschemes/multitude-light.sh
@@ -0,0 +1,35 @@
+# Multitude colorscheme with light background
+#
+# by Renato Alves (alves.rjc@gmail.com)
+
+background="#eeeeee"
+foreground="#000000"
+cursor="#000000"
+mouse_background="#ffffff"
+mouse_foreground="#000000"
+highlight="#dddddd"
+border="#cccccc"
+
+ color0="#000000"  # black
+ color8="#666666"  # brblack
+
+ color1="#9e1828"  # red
+ color9="#cf6171"  # brred
+
+ color2="#008800"  # green
+color10="#7cbc8c"  # brgreen
+
+ color3="#968a38"  # yellow
+color11="#d4cd7d"  # bryellow
+
+ color4="#414171"  # blue
+color12="#4186be"  # brblue
+
+ color5="#963c59"  # magenta
+color13="#cf9ebe"  # brmagenta
+
+ color6="#418179"  # cyan
+color14="#71bebe"  # brcyan
+
+ color7="#a1a1a1"  # white
+color15="#ffffff"  # brwhite
