75a125464858ad879bd7ca48813d4794bee9a45d
thawk <thawk009@gmail.com>
2022-05-06 22:53:53 +0800

    update

--- homebrew/bootstrap.sh
+++ homebrew/bootstrap.sh
@@ -2,8 +2,11 @@
 # Time: 2022-04-08 22:12:02
 
 env_file="${DOTFILES_LOCAL}/homebrew/env.sh"
+path_file="${DOTFILES_LOCAL}/homebrew/path.sh"
+
 mkdir -p "$(dirname "$env_file")"
 rm -f "$(dirname "$env_file")"/*
 # : > "${env_file}"
 
-brew shellenv > "${env_file}"
+brew shellenv | grep -v "\bPATH=" > "${env_file}"
+brew shellenv | grep "\bPATH=" > "${path_file}"
