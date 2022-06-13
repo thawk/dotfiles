b20539271e7db06fa7ac843aff758f273826a827
thawk <thawk009@gmail.com>
2022-06-13 11:32:58 +0800

    support SSH agent on WSL2

--- keeagent/requirements.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-[[ "${OSTYPE}" == "linux-gnu" ]] && \
-    grep "Microsoft\|WSL" /proc/sys/kernel/osrelease > /dev/null && \
-    type socat > /dev/null
---
+++ wsl/README.md
@@ -0,0 +1,4 @@
+- Refered to: https://gist.github.com/strarsis/e533f4bca5ae158481bbe53185848d49
+
+- MUST have `socat` installed in WSL
+
