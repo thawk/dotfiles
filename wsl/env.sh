c798b0fa93675c5f67401aea1c793b1c67d29cdb
thawk <thawk009@gmail.com>
2022-06-23 13:48:42 +0800

    MY_SOCKS5_PROXY以.localrc为准

--- wsl/env.sh
+++ wsl/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Time: 2022-06-23 11:09:23
 
-export MY_SOCKS5_PROXY=$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):1080
+export MY_SOCKS5_PROXY=${MY_SOCKS5_PROXY:-$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):1080}
