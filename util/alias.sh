a3f5e184e725f639c769b4ddb4d9058b2924d5f6
thawk <thawk009@gmail.com>
2022-07-06 11:14:38 +0800

    兼容ncat

--- util/alias.sh
+++ util/alias.sh
@@ -1,5 +1,5 @@
 # Use a local proxy
-alias ap="env all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -X 5 -x \${MY_SOCKS5_PROXY:-127.0.0.1:1080} %h %p\"' "
+alias ap="env all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} GIT_SSH_COMMAND='ssh -o ProxyCommand=\"nc -x \${MY_SOCKS5_PROXY:-127.0.0.1:1080} %h %p\"' "
 #alias aph="env all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} "
 alias aph="all_proxy=socks5://\${MY_SOCKS5_PROXY:-127.0.0.1:1080} "
 # Use a local proxy
--- util/util.sh
+++ util/util.sh
@@ -10,7 +10,7 @@ setproxy() {
     # 如果命令行没有提供代理地址，使用MY_SOCKS5_PROXY的值，如果也没有设置就用127.0.0.1:1080
     local proxy=${1:-${MY_SOCKS5_PROXY:-127.0.0.1:1080}}
     export all_proxy=socks5://${MY_SOCKS5_PROXY:-127.0.0.1:1080}
-    export GIT_SSH_COMMAND="ssh -o ProxyCommand=\"nc -X 5 -x ${MY_SOCKS5_PROXY:-127.0.0.1:1080} %h %p\""
+    export GIT_SSH_COMMAND="ssh -o ProxyCommand=\"nc -x ${MY_SOCKS5_PROXY:-127.0.0.1:1080} %h %p\""
 }
 
 resetproxy() {
