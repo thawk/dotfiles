a508944290837e40dfbffe648b5082944fc29543
thawk <thawk009@gmail.com>
2022-07-13 17:55:14 +0800

    update

--- util/bin/backup-spacevim.sh
+++ util/bin/backup-spacevim.sh
@@ -4,4 +4,5 @@ DEST_FILE=${1:-spacevim-$(date +%Y%m%d).tar.bz2}
 
 dirs=(.SpaceVim.d .SpaceVim .cache/vimfiles )
 [[ -d "$HOME/.config/coc" ]] && dirs=( "${dirs[@]}" .config/coc )
-tar -C "$HOME" -cjvf "${DEST_FILE}" --exclude .git --exclude "*.so" "${dirs[@]}"
+
+tar -C "$HOME" -cjvf "${DEST_FILE}" --exclude .git --exclude ".cache/*.so" "${dirs[@]}"
