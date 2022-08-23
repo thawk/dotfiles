bfde8fe3ad1784e235cb0b549efbbe85609e0eaf
thawk <thawk009@gmail.com>
2022-08-23 09:58:43 +0800

    update

--- wsl/env.sh
+++ wsl/env.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Time: 2022-06-23 11:09:23
 
-export LOCAL_SOCKS5_PROXY=${LOCAL_SOCKS5_PROXY:-$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):1080}
+#export LOCAL_SOCKS5_PROXY=${LOCAL_SOCKS5_PROXY:-$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):1080}
+export LOCAL_SOCKS5_PROXY=${LOCAL_SOCKS5_PROXY:-$(hostname).local:1080}
