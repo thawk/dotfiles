7f98e8a05c3732196a5438db2869df43cc3b8d75
thawk <thawk009@gmail.com>
2023-01-06 17:35:21 +0800

    wsl去掉带空格的PATH条目

--- wsl/env.sh
+++ wsl/env.sh
@@ -3,3 +3,5 @@
 
 #export LOCAL_SOCKS5_PROXY=${LOCAL_SOCKS5_PROXY:-$(grep -m 1 nameserver /etc/resolv.conf | awk '{print $2}'):1080}
 export LOCAL_SOCKS5_PROXY=${LOCAL_SOCKS5_PROXY:-$(hostname).local:1080}
+# Remove paths that contains space, e.g. C:/Program Files/
+export PATH="$(echo "$PATH" | tr ':' '\n' | grep -v ' ' | tr '\n' ':' | sed 's/:$//')"
