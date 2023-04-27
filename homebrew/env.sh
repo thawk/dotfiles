7cc5f8ed494fe543dd87e9060a5ecb26778b2ccc
thawk <thawk009@gmail.com>
2023-04-27 17:03:50 +0800

    update

--- homebrew/env.sh
+++ homebrew/env.sh
@@ -2,4 +2,4 @@
 #export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 #export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
-export HOMEBREW_FORCE_BREWED_CURL=1
+export HOMEBREW_FORCE_BREWED_CURL=0
