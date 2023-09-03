eca9e52165f6734bd302ac87fbf932f5a748776c
thawk <thawk009@gmail.com>
2023-09-03 11:01:37 +0800

    update

--- util/bin/backup-spacevim.sh
+++ util/bin/backup-spacevim.sh
@@ -2,7 +2,13 @@
 
 DEST_FILE=${1:-spacevim-$(date +%Y%m%d).tar.bz2}
 
-files=(.SpaceVim.d .SpaceVim .cache/vimfiles )
+if [ -z "$XDG_CONFIG_HOME" ]; then
+    SPACEVIM_D_DIR="$XDG_CONFIG_HOME/SpaceVim.d"
+else
+    SPACEVIM_D_DIR="$HOME/.SpaceVim.d"
+fi
+
+files=("${SPACEVIM_D_DIR}" .SpaceVim .cache/vimfiles )
 [[ -d "$HOME/.config/coc" ]] && files=( "${files[@]}" .config/coc )
 
 tar -C "$HOME" -cjvf "${DEST_FILE}" --exclude .git --exclude ".cache/*.so" --exclude "*/node_modules/*" "${files[@]}"
