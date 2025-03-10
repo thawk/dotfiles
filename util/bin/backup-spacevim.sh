c5d2df6998ecd3c040816e37c23e0a6b45addd5d
thawk <thawk009@gmail.com>
2025-03-10 17:21:24 +0800

    fix backup-spacevim.sh

--- util/bin/backup-spacevim.sh
+++ util/bin/backup-spacevim.sh
@@ -2,10 +2,10 @@
 
 DEST_FILE=${1:-spacevim-$(date +%Y%m%d).tar.bz2}
 
-if [ -z "$XDG_CONFIG_HOME" ]; then
-    SPACEVIM_D_DIR="$XDG_CONFIG_HOME/SpaceVim.d"
+if [[ -n "$XDG_CONFIG_HOME" ]]; then
+    SPACEVIM_D_DIR="${XDG_CONFIG_HOME#$HOME/}/SpaceVim.d"
 else
-    SPACEVIM_D_DIR="$HOME/.SpaceVim.d"
+    SPACEVIM_D_DIR=".SpaceVim.d"
 fi
 
 files=("${SPACEVIM_D_DIR}" .SpaceVim .cache/vimfiles )
