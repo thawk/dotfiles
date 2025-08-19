42744f3efcb3be2c2d6442618058aa3e959035ce
thawk <thawk009@gmail.com>
2025-08-19 19:10:26 +0800

    update

--- fzf/bootstrap.sh
+++ fzf/bootstrap.sh
@@ -5,3 +5,5 @@ init_plugin "fzf"
 
 echo "[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh" >> "$(create_plugin_file env.zsh)"
 echo "[ -f ~/.fzf.bash ] && source ~/.fzf.bash" >> "$(create_plugin_file env.bash)"
+echo "export PERCOL=fzf" >> "$(create_plugin_file env.zsh)"
+echo "export PERCOL=fzf" >> "$(create_plugin_file env.bash)"
