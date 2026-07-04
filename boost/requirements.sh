a2db49ea3bcc38536c88125ff56bb88f54bf1c69
thawk <thawk009@gmail.com>
2026-07-04 17:50:34 +0800

    改进对b2的检测

--- boost/requirements.sh
+++ boost/requirements.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-my_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+my_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PATH=${PATH/:${my_DIR}\/bin/}
 
-if type b2 &> /dev/null
-then
-    # use exists b2 command
-    false
-else
-    [[ -d "$HOME/workspace" ]] && [[ -n "$(find "$HOME/workspace" -maxdepth 1 -type d -name boost -o -name "boost_*")" ]]
-fi
+CMD_PATH=$(type -P "b2" 2>/dev/null)
 
+if [[ -n "$CMD_PATH" && "$CMD_PATH" != "$my_DIR/bin/b2" ]]; then
+  # use exists b2 command
+  false
+else
+  [[ -d "$HOME/workspace" ]] && [[ -n "$(find "$HOME/workspace" -maxdepth 1 -type d -name boost -o -name "boost_*")" ]]
+fi
